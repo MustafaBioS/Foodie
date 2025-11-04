841a264db7870327e78a9c80be37217445324666
MustafaBioS <bioshpere.voices@gmail.com>
2025-11-04 17:10:27 +0200

    FIX - Pause menu not working after continuing

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -3,6 +3,8 @@ extends CharacterBody2D
 @onready var fade = $"../Fade"
 @onready var win = $"../Win"
 @onready var pause = $"../Pause"
+@onready var btns = $"../Pause/BtnCon"
+@onready var title = $"../Pause/PauseTitle"
 
 const JUMP_VELOCITY = -400.0
 
@@ -13,6 +15,7 @@ func _ready() -> void:
 	win.visible = false
 	pause.visible = false
 	State.paused = false
+	State.win == false
 	anim.play("FadeIn")
 	await get_tree().create_timer(1.0).timeout
 	fade.visible = false
@@ -22,12 +25,16 @@ func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("pause") and State.paused == false and State.options == false and State.shop == false and State.win == false:
 		State.paused = true
 		pause.visible = true
+		btns.visible = true
+		title.visible = true
 		print(State.paused)
 		print(pause.visible)
 		
 	elif Input.is_action_just_pressed("pause") and State.paused == true and State.options == false and State.shop == false and State.win == false:
 		State.paused = false
 		pause.visible = false
+		pause.visible = false
+		title.visible = false
 		print(State.paused)
 		print(pause.visible)
 
